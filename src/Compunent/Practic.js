b45fe035cce77ea47511cc29d00881d62b947889
Mostafa Mohsin <mostafamohsin783@gmail.com>
2023-12-02 15:01:10 +0600

    new porject add

--- src/Compunent/Practic.js
+++ src/Compunent/Practic.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 const Practic = () => {
 const [time,setTime] = useState(60);
-
+const [backgroundColor,setBackgroundColor] = useState('green')
 useEffect(() => {
 
     if(time > 0){
@@ -11,12 +11,30 @@ useEffect(() => {
     }
 },[time])
 
+const changeBgColor = () => {
+  const bgColorChange = backgroundColor === "tomato" ? "orange" : 'black';
+  setBackgroundColor(bgColorChange);
+}
+
     return (
         <div>
           <h1 style={{textAlign: 'center'}}>Practice Section</h1>  
 
 
       <h1>Time Left : {time} second.</h1>
+<br />
+
+<button onClick={changeBgColor}>Change Color</button>
+
+<div style={{
+    backgroundColor,
+    height: "200px",
+    width : "200px",
+    cursor:"pointer"
+}}>
+Click for Change backgroundColor
+</div>
+
 
         </div>
     );
